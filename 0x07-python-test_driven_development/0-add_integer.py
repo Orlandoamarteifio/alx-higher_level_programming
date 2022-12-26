9edc2433e4facacf89c2c5d42485f329717b64eb
Orlando amarteifio <seniorcorradi1234@gmail.com>
2022-12-26 14:11:38 -0800

    function that adds two integers

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -12,13 +12,13 @@ def add_integer(a, b=98):
     Args:
         a: first number
         b: second number
-        
+
     Returns:
        The addition of the two given numbers
-       
+
     Raises:
         TypeError: If a or b aren't integer and/or float numbers
-        
+
     """
 
     if not isinstance(a, int) and not isinstance(a, float):
